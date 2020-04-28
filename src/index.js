24135d713587bf021306951a2b0e2230f5f8bca4
Sara Kay <sss.22@dartmouth.edu>
2020-04-27 16:44:14 -1000

    added routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import './style.scss';
-import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-let time = 0;
-setInterval(() => {
-  time += 1;
-  const message = `You've been on this page for ${time.toString()} seconds.`;
-  $('#main').html(message);
-}, 1000);
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/test/:id" component={Test} />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('main'));
