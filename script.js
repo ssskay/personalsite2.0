9d1e1ac3a7cdd7a9fe2626b1e0b0be2caa7e54a5
Sara Kay <sss.22@dartmouth.edu>
2020-05-17 15:05:24 -1000

    styling time

--- script.js
+++ script.js
@@ -8,18 +8,20 @@ const state = {
 
 const ideaPics = [
   'Pictures/Idea/arblee.png',
-  'Pictures/Idea/coda.png',
   'Pictures/Idea/officehours.png',
+  'Pictures/Idea/studykit.png',
+  'Pictures/Idea/coda.png',
   'Pictures/Idea/zoomu.png',
-  'Pictures/Idea/studykit.png'
+  'Pictures/Idea/ideas.png',
 ];
 
 const ideaDescrip = [
-  'ARblee - Consulting retailers to use augmented reality in their physical storefronts to draw people in; arose from Pokemon Go! hype, currently disbanded since retail is dying',
-  'Cult of Dope Automations - I love everything super cute, like alpacas, pink, poros (on the heart), and my boyfriend! I got this super cute icon commissioned for me and him for Valentines Day <3',
-  'Office Hours - I can be professional if I need to! (even though this is the only professional headshot I have, from my junior year function 4 years ago)',
-  'Zoom University - I love everything super cute, like alpacas, pink, poros (on the heart), and my boyfriend! I got this super cute icon commissioned for me and him for Valentines Day <3',
-  'Study Kit - I can be professional if I need to! (even though this is the only professional headshot I have, from my junior year function 4 years ago)',
+  '<strong>ARblee (2016):</strong> Platform for retailers to use augmented reality in their physical storefronts during the peak of Pokemon Go! hype <br> <strong>Status: </strong> Disbanded since retail is dying',
+  '<strong>Office Hours (2018):</strong> Organzied archieve of class syllabi, textbooks, exams, office hour notes and more <br> <strong>Status: </strong> Disbanded due to issues onboarding professors and content copyright',
+  '<strong>Study Kit (2019): </strong>Termly subscription box that comes with essential school supplies and pre-made study material to make back-to-school shopping easier  <br> <strong>Status: </strong>Disbanded due to similarities to exisiting competition',
+  '<strong>Cult of Dope Automations (CoDA) (2019): </strong> Brand that encourages others to use automation tools to make their lives easier <br> <strong>Status: </strong> Currently working on pre-made automations for the elderly and startups',
+  '<strong>Zoom University (2020):</strong> Parody brand of Zoom to raise money for COVID-19 relief by commissioning artists who have lost their convention income <br> <strong>Status: </strong> I have a lot of fake Zoom University merch and a storefront coming Summer 2020',
+  '<strong>Literally all of them:</strong> I brainstorm startup business plans for fun and would love the oppertunity to grow my technicals skills at DALI and help others make their startup dreams come true.',
 ];
 
 
@@ -30,17 +32,22 @@ const headPics = [
 ];
 
 const headDescrip = [
-  'I absolutely love anime and what better way to express that than a kawaii anime icon of me?!',
-  'I love everything super cute, like alpacas, pink, poros (on the heart), and my boyfriend! I got this super cute icon commissioned for me and him for Valentines Day <3',
-  'I can be professional if I need to! (even though this is the only professional headshot I have, from my junior year function 4 years ago)',
+  "<strong>An anime fanatic:</strong> What better way to express that than a kawaii anime icon of me?! It helps that I actually super happy like an anime girl since I can truly express myself at Dartmouth with friends at DALI",
+  '<strong>A cute girl:</strong> I love everything cute!! My favorites include alpacas, the color pink pink, poros (on the heart), and my boyfriend! I got matching icons commissioned for us on Valentines Day <3',
+  "<strong>A 'normal' person:</strong> I figured I should throw in a professional headshot, even though it's from my junior year function four years ago.",
+
 ];
 
 const bodyPics = [
   'Pictures/body/bathrobe.png',
+  'Pictures/body/canadagoose.jpg',
+  'Pictures/body/magicalgirl.png',
 ];
 
 const bodyDescrip = [
-  'I love working from home in comfort',
+  "<strong>Home:</strong> I'm determine to work from home full time in the future so I can code in maximum fluffy comfort. This is also how I feel internally thinking about working in DALI's chill and cozy environment! :D",
+  "<strong>Dartmouth:</strong> My fragile Hawaii/Singapore-raised body would not last a single day at Dartmouth without this jacket by my side",
+  "<strong>An Anime Convention: </strong>Mandatory Comic-Con/Anime Expo attire. My closet is great to raid for flair during any spontaneous occassion",
 ];
 
 const accPics = [
@@ -48,15 +55,15 @@ const accPics = [
 ];
 
 const accDescrip = [
-  "My lit baby, he's quite cute",
+  "<strong>Flare:</strong> My baby plush alpaca who I share with my boyfriend. Debugging bff. He's the cutest lit baby ever",
+  "<strong>Beats Headphones:</strong> You will literally never see me without these on. I need music to survive in the outside world. Playlists range from anime soundtracks to K-pop and classic jazz",
+  "<strong>Nintendo Switch:</strong> Would you believe me if I told you that I've met most of my friends at Dartmouth through defeating them in Super Smash Bros?",
+  "<strong>My laptop:</strong> Am I streaming chill beats to study to, designing new stickers or coding dope projects for DALI? Hire me to find out ;)",
 ];
 
 
-
-
 function newIdea() {
-  let fullIdeaDescription = 'Startup: ';
-  fullIdeaDescription += ideaDescrip[state.idea];
+  let fullIdeaDescription = ideaDescrip[state.idea];
   const output = document.getElementById('Idea Description');
   output.innerHTML = fullIdeaDescription;
 }
@@ -86,8 +93,7 @@ function updateIdeaLeft() {
 }
 
 function newHead() {
-  let fullHeadDescription = 'head: ';
-  fullHeadDescription += headDescrip[state.head];
+  let fullHeadDescription = headDescrip[state.head];
   const output = document.getElementById('Head Description');
   output.innerHTML = fullHeadDescription;
 }
@@ -117,8 +123,7 @@ function updateHeadLeft() {
 }
 
 function newBody() {
-  let fullBodyDescription = 'body: ';
-  fullBodyDescription += bodyDescrip[state.head];
+  let fullBodyDescription = bodyDescrip[state.body];
   const output = document.getElementById('Body Description');
   output.innerHTML = fullBodyDescription;
 }
@@ -148,8 +153,7 @@ function updateBodyLeft() {
 }
 
 function newAcc() {
-  let fullAccDescription = 'body: ';
-  fullAccDescription += accDescrip[state.head];
+  let fullAccDescription = accDescrip[state.acc];
   const output = document.getElementById('Accessory Description');
   output.innerHTML = fullAccDescription;
 }
